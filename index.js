647e2c0ceb8604ce8907526c934b98d0fc645884
johnshelb <jsnd11@gmail.com>
2017-03-18 23:03:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+// function getFirstSelector(selector) {
+//   return document.querySelector(selector)
+// }
+//
+// function nestedTarget() {
+//   return document.getElementsByClassName('target')[0]
+// }
+//
+// function increaseRankBy(n) {
+//   var listItems = document.querySelectorAll('ul.ranked-list li')
+//   for (let i = 0, l = listItems.length;i<l;i++){
+//     listItems[i].innerHTML = (parseInt(listItems[i].innerHTML) + n).toString()
+//   }
+// }
+//
+// function deepestChild() {
+//   var grandNode = document.getElementById('grand-node');
+//   var nodeList = grandNode.querySelectorAll('div');
+//   return nodeList[nodeList.length-1]
+// }
+
 function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
 function nestedTarget() {
-  return document.getElementsByClassName('target')[0]
+  return document.querySelector('#nested .target')
 }
 
 function increaseRankBy(n) {
-  var listItems = document.querySelectorAll('ul.ranked-list li')
-  for (let i = 0, l = listItems.length;i<l;i++){
-    listItems[i].innerHTML = (parseInt(listItems[i].innerHTML) + n).toString()
+  const x = document.querySelectorAll('.ranked-list li')
+  for(let i = 0; i <x.length; i++){
+    x[i].innerHTML = parseInt(x[i].innerHTML) + n
   }
 }
 
 function deepestChild() {
-  var grandNode = document.getElementById('grand-node');
-  var nodeList = grandNode.querySelectorAll('div');
-  return nodeList[nodeList.length-1]
+  const grandNode = document.querySelector('#grand-node')
+  const divList = grandNode.querySelectorAll('div')
+  debugger;
+  return divList[divList.length - 1]
 }
